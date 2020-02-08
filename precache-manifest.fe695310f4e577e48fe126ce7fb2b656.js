a6f400282f18fc660eb8aa43e9e7ca81f9e84edc
Hibah Shafi <hibahshafi.hs@gmail.com>
2020-02-08 14:06:44 -0600

    Updates

---
+++ precache-manifest.fe695310f4e577e48fe126ce7fb2b656.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7848751418c6f80b37f3e11dbcfc0189",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "8a571c550ebf88b585db",
+    "url": "/ClickyGame/static/css/main.cd74283c.chunk.css"
+  },
+  {
+    "revision": "7db36e5fb44eae9e6752",
+    "url": "/ClickyGame/static/js/2.3f1aee96.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/ClickyGame/static/js/2.3f1aee96.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8a571c550ebf88b585db",
+    "url": "/ClickyGame/static/js/main.caeb0040.chunk.js"
+  },
+  {
+    "revision": "802993730bb9c6f1d6e8",
+    "url": "/ClickyGame/static/js/runtime-main.1b3f0fd3.js"
+  }
+]);
